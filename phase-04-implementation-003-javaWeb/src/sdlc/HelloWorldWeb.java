6bb9228243d4c7f04b93d1f8166987562e68b9ac
赵科淙 <@10.14.32.156>
2019-06-26 21:47:12 +0800

    worker：adm-D加入project

---
+++ phase-04-implementation-003-javaWeb/src/sdlc/HelloWorldWeb.java
@@ -0,0 +1,16 @@
+package sdlc;
+
+public class HelloWorldWeb {
+    public  String poemTest="《\"善待地球\"》<br /> <span style=\"font-size: 18px; color: grey\">\r\n" + 
+    		"			-赵科淙 </span><br /> \r\n" + 
+    		"			<br /> 善待地球就是善待自己\r\n" + 
+    		                 "拯救地球就是拯救未来 \r\n" + 
+    		"			<br /> 但存方寸地，留与子孙耕";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public String getPoemContent() {
+    	return poemTest;
+    }
+}
